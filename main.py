5cba7c4c667b140bd11831bdc51f7ba338480813
cynthia-k2 <0907cincinnati@gmail.com>
2025-02-08 13:29:07 -0800

    tehe

--- main.py
+++ main.py
@@ -99,7 +99,7 @@ while running:
             print(f"Axis 0: {axis_0}, Axis 1: {axis_1}, Axis 2:{axis_2}, Axis 3: {axis_3}")
             #print(json_data)
             print(f"Axis 0: {axis_0_pwm_value}, Axis 1: {axis_1_pwm_value}, Axis 2: {axis_2_pwm_value}, Axis 3: {axis_3_pwm_value}")
-            calculate_rotation_thrusters()
+            # calculate_rotation_thrusters()
 
             time.sleep(0.005)
 
