6b20e0d0a77a1e0360fb53d7a80b5a8428c77119
cynthia-k2 <0907cincinnati@gmail.com>
2025-02-15 17:07:04 -0800

    comment out work

--- main.py
+++ main.py
@@ -59,40 +59,50 @@ while running:
             axis_2_rotation = int((axis_2)*100)
             axis_3_rotation = int((axis_3)*100)
 
-            joystick_values = [axis_0, axis_1, axis_2, axis_3]
+            # joystick_values = [axis_0, axis_1, axis_2, axis_3]
 
-            thruster_1 = 1
-            thruster_2 = 2
-            thruster_3 = 3
-            thruster_4 = 4
-            thruster_5 = 5
+            # thruster_1 = 1
+            # thruster_2 = 2
+            # thruster_3 = 3
+            # thruster_4 = 4
+            # thruster_5 = 5
 
-            thrusters = [thruster_1, thruster_2, thruster_3, thruster_4, thruster_5]
+            # thrusters = [thruster_1, thruster_2, thruster_3, thruster_4, thruster_5]
 
-            rotation = 100/thrusters
+            # rotation = 100/thrusters
 
-            thruster_1_new = rotation * thruster_1
-            thruster_2_new = rotation * thruster_2
+            # thruster_1_new = rotation * thruster_1
+            # thruster_2_new = rotation * thruster_2
 
-            thrusters_percent_ideal = [thruster_1, thruster_2, thruster_3, thruster_4, thruster_5]
+            # thrusters_percent_ideal = [thruster_1, thruster_2, thruster_3, thruster_4, thruster_5]
 
-            thrusters_max_percent = [thruster_1_b, thruster_2_b, thruster_3_b, thruster_4_b, thruster_5_b]
+            # thruster_1_b = 1
+            # thruster_2_b = 2
+            # thruster_3_b = 3
+            # thruster_4_b = 4
+            # thruster_5_b = 5
 
-            
+            # thrusters_max_percent = [thruster_1_b, thruster_2_b, thruster_3_b, thruster_4_b, thruster_5_b]
 
-            added_values = int(axis_0_rotation + axis_2_rotation)
-            print("x and r:", added_values)
+            # abs(thruster_1_b)
+            # abs(thruster_2_b)
+            # abs(thruster_3_b)
+            # abs(thruster_4_b)
+            # abs(thruster_5_b)
 
-            if int(added_values > 100 or added_values < 0):
-                power_limit = int(added_values/(added_values/100))
-                power_limit = max(100, min(-100, power_limit))
-                print(power_limit)
-            elif int(added_values < 100):
-                print(added_values)
-            elif int(added_values = 100):
-                print(added_values)
-            else:
-                print("Something went wrong")
+            # added_values = int(axis_0_rotation + axis_2_rotation)
+            # print("x and r:", added_values)
+
+            # if int(added_values > 100 or added_values < 0):
+            #     power_limit = int(added_values/(added_values/100))
+            #     power_limit = max(100, min(-100, power_limit))
+            #     print(power_limit)
+            # elif int(added_values < 100):
+            #     print(added_values)
+            # elif int(added_values = 100):
+            #     print(added_values)
+            # else:
+            #     print("Something went wrong")
 
             axis_0_pwm_value = joystick_to_pwm(axis_0)
             axis_1_pwm_value = joystick_to_pwm(axis_1)
