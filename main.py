1100ae5e753b8f4fb38391afb7ec38f41010dda6
cynthia-k2 <0907cincinnati@gmail.com>
2025-02-15 16:59:06 -0800

    rough outline of big code (not finished)

--- main.py
+++ main.py
@@ -55,7 +55,31 @@ while running:
             axis_3 = joystick.get_axis(3)
 
             axis_0_rotation = int((axis_0)*100) 
+            axis_1_rotation = int((axis_1)*100)
             axis_2_rotation = int((axis_2)*100)
+            axis_3_rotation = int((axis_3)*100)
+
+            joystick_values = [axis_0, axis_1, axis_2, axis_3]
+
+            thruster_1 = 1
+            thruster_2 = 2
+            thruster_3 = 3
+            thruster_4 = 4
+            thruster_5 = 5
+
+            thrusters = [thruster_1, thruster_2, thruster_3, thruster_4, thruster_5]
+
+            rotation = 100/thrusters
+
+            thruster_1_new = rotation * thruster_1
+            thruster_2_new = rotation * thruster_2
+
+            thrusters_percent_ideal = [thruster_1, thruster_2, thruster_3, thruster_4, thruster_5]
+
+            thrusters_max_percent = [thruster_1_b, thruster_2_b, thruster_3_b, thruster_4_b, thruster_5_b]
+
+            
+
             added_values = int(axis_0_rotation + axis_2_rotation)
             print("x and r:", added_values)
 
@@ -68,12 +92,15 @@ while running:
             elif int(added_values = 100):
                 print(added_values)
             else:
-                print("something went wrong")
+                print("Something went wrong")
 
             axis_0_pwm_value = joystick_to_pwm(axis_0)
             axis_1_pwm_value = joystick_to_pwm(axis_1)
             axis_2_pwm_value = joystick_to_pwm(axis_2)
             axis_3_pwm_value = joystick_to_pwm(axis_3)
+
+            print(f"Raw Values: Axis 0: {axis_0}, Axis 1: {axis_1}, Axis 2:{axis_2}, Axis 3: {axis_3}")
+            print(f"PWM Values: Axis 0: {axis_0_pwm_value}, Axis 1: {axis_1_pwm_value}, Axis 2: {axis_2_pwm_value}, Axis 3: {axis_3_pwm_value}")
 
             pwm_values = {
                 'x': axis_0_pwm_value,
@@ -84,9 +111,6 @@ while running:
 
             json_data = json.dumps(pwm_values)
             client_socket.sendall(json_data.encode('utf-8'))
-
-            print(f"Raw Values: Axis 0: {axis_0}, Axis 1: {axis_1}, Axis 2:{axis_2}, Axis 3: {axis_3}")
-            print(f"PWM Values: Axis 0: {axis_0_pwm_value}, Axis 1: {axis_1_pwm_value}, Axis 2: {axis_2_pwm_value}, Axis 3: {axis_3_pwm_value}")
 
             time.sleep(0.005)
 
