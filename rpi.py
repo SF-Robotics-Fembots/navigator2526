dad5c31ccaab2ec62a0721ca0af685cab1d84906
cynthia-k2 <0907cincinnati@gmail.com>
2025-03-08 16:05:56 -0800

    little cleanup

--- rpi.py
+++ rpi.py
@@ -1,4 +1,4 @@
-import socket, requests, time, json
+import socket, time, json
 import RPi.GPIO as GPIO # type: ignore
 
 host_ip = '10.0.0.8' 
@@ -7,10 +7,6 @@ port = 8080
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((host_ip, port))
 print("1")
-#s.sendall(b"hello, world")
-#print("...")
-#data = s.recv(1024)
-#print(f"recieved message: ")
 
 thruster_5 = 0
 thruster_4 = 0
@@ -18,28 +14,15 @@ thruster_3 = 0
 thruster_2 = 0
 thruster_1 = 0
 
-servoPIN = [14, 1, 8, 15, 0]
-GPIO.setmode(GPIO.BCM)
+servoPIN = [14, 1, 8, 15, 0] #define GPIO pins for thrusters
+GPIO.setmode(GPIO.BCM) #initialize GPIO
 for pin in servoPIN:
     GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 100) #100 Hz frequency
 
-# def set_thrusters_pwms(pwm_values):
-#     for thruster, pwm_value in zip(thrusters, pwm_values):
-#         thruster.ChangeDutyCycle(pwm_value)
-
-# def get_pwm_value():
-#         try:
-#         SERVER_URL = ""
-#         response = requests.get(SERVER_URL)
-#         response.raise_for_status()
-#         pwm_value = response.json().get('pwm_values')
-#         if 1000 <= pwm_value <= 2000:
-#             return pwm_value
-#         else:
-#             print("PWM value out of range")
-#             return None
-
+def set_thrusters_pwms(pwm_values, thrusters):
+    for thruster, pwm_value in zip(thrusters, pwm_values):
+        thruster.ChangeDutyCycle(pwm_value)
 
 p.start(2.5) # Initialization
 datain = client_socket.recv(1024)
@@ -57,13 +40,11 @@ while True:
     print("D", data)
     json_data = data.decode('utf-8')
     pwm_values = json.loads(json_data)
-    # pwm_string = data.decode('utf-8')
-    # pwm_values = list(map(float, pwm_string.split(',')))
     print("received pwm values:", pwm_values)
 
-
-
     thrusters = [thruster_5, thruster_4, thruster_3, thruster_2, thruster_1]
+
+    set_thrusters_pwms(pwm_values, thrusters)
 
     for thruster in thrusters:
         pass
@@ -77,3 +58,23 @@ while True:
         #figure out how to exit porgram with command
     except:
         GPIO.cleanup()
+
+# def get_pwm_value():
+#         try:
+#         SERVER_URL = ""
+#         response = requests.get(SERVER_URL)
+#         response.raise_for_status()
+#         pwm_value = response.json().get('pwm_values')
+#         if 1000 <= pwm_value <= 2000:
+#             return pwm_value
+#         else:
+#             print("PWM value out of range")
+#             return None
+
+#s.sendall(b"hello, world")
+#print("...")
+#data = s.recv(1024)
+#print(f"recieved message: ")
+
+# pwm_string = data.decode('utf-8')
+# pwm_values = list(map(float, pwm_string.split(',')))
