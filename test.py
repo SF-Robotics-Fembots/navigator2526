cdf915ed594b5dd66523e6eef86d788787174aae
cynthia-k2 <0907cincinnati@gmail.com>
2025-04-21 14:30:07 -0700

    test.py

---
+++ test.py
@@ -0,0 +1,73 @@
+import board
+import busio
+import pwmio
+#import RPi.GPIO as GPIO # type: ignore
+import adafruit_pca9685 #import PCA9685
+from adafruit_servokit import ServoKit
+import socket, time, json, sys
+
+#set thrusters on bottomside
+thruster_5 = 0 #5
+thruster_4 = 0 #4
+thruster_3 = 0 #3
+thruster_2 = 0 #2
+thruster_1 = 0 #1
+thrusters = [thruster_5, thruster_4, thruster_3, thruster_2, thruster_1]
+
+i2c = busio.I2C(board.SCL, board.SDA)
+print("{i2c}")
+listofdev = i2c.scan()
+print(listofdev)
+
+i2c = board.I2C() # uses board.SCL and board.SDA
+shield = adafruit_pca9685.PCA9685(i2c)
+kit = ServoKit(channels=16)
+shield.frequency = 98 #was 100
+
+while True:
+    for x in range(90, 101):
+        shield.frequency = x
+
+        thrusterChannel5 = shield.channels[14]
+        thrusterChannel4 = shield.channels[1]
+        thrusterChannel3 = shield.channels[8]
+        thrusterChannel2 = shield.channels[15]
+        thrusterChannel1 = shield.channels[2]
+        thrusterChannel5.duty_cycle = 0x2666
+
+        pwm_values = [1000, 1500, 2000]
+
+        #5
+        throttlePW = int((pwm_values[1]/10000*65536)*(x/100))
+        thrusterChannel5.duty_cycle = throttlePW
+        time.sleep(0)
+
+        #4
+        throttlePW = int((pwm_values[1]/10000*65536)*(x/100))
+        thrusterChannel4.duty_cycle = throttlePW
+        time.sleep(0)
+
+
+        #3
+        throttlePW = int((pwm_values[1]/10000*65536)*(x/100))
+        thrusterChannel3.duty_cycle = throttlePW
+        time.sleep(0)
+
+
+        #2
+        throttlePW = int((pwm_values[1]/10000*65536)*(x/100))
+        thrusterChannel2.duty_cycle = throttlePW
+        time.sleep(0)
+
+
+        #1
+        throttlePW = int((pwm_values[1]/10000*65536)*(x/100))
+        thrusterChannel1.duty_cycle = throttlePW
+        time.sleep(0)
+
+        
+        try:
+            time.sleep(0.005)
+        except KeyboardInterrupt:
+            sys.exit()
+            #figure out how to exit porgram with command
