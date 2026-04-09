ba097bf567965f3f5d63bb4056f458e9e304eb3b
not-emily-0801 <29nguyen@stfrancishs.org>
2026-04-09 12:10:21 -0700

    update cameras script

--- cameras.sh
+++ cameras.sh
@@ -1,3 +1,4 @@
+
 ustreamer --device=/dev/video0 --host=192.168.1.68 --format=MJPEG --port=8080 --device-timeout 2 -r 800x600 -b 2 --workers 2 --encoder=HW -n & sleep 2 &
 ustreamer --device=/dev/video2 --host=192.168.1.68 --format=MJPEG --port=8081 --device-timeout 2 -r 800x600 -b 2 --workers 2 --encoder=HW -n & sleep 2 &
 ustreamer --device=/dev/video4 --host=192.168.1.68 --format=MJPEG --port=8082 --device-timeout 2 -r 800x600 -b 2 --workers 2 --encoder=HW -n & sleep 2 &
